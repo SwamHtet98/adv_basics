6e205e401f8dc72a7e96ed6246250b386ed481da
SwamHtet98 <swamhtetnaing25@gmail.com>
2025-01-28 23:11:22 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:adv_basics/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168)
+            ], begin: Alignment.bottomRight, end: Alignment.topLeft),
+          ),
+          child: const HomeScreen()),
+    ),
+  ));
+}
